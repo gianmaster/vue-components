082961a94b12eefb9137eaa10a42b91de01a276d
Giancarlos Cercado <giancarloscercado@gmail.com>
2017-02-14 01:19:49 -0500

    initial commit

---
+++ src/components/Typeahead.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="typeahead">
+        <div class="typeahead__toggle">
+            <input type="text" class="tepeahead__search" placeholder="Input some text" ref="search">
+            <span class="typeahead__text" ref="text">Some text</span>
+        </div>
+        <ul class="typeahead__list">
+            <li class="typeahead__item">
+                <a class="typeahead__link">Item 1</a>
+            </li>
+            <li class="typeahead__item">
+                <a class="typeahead__link">Item 2</a>
+            </li>
+            <li class="typeahead__item">
+                <a class="typeahead__link">Item 3</a>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+
+    export default {
+        name: 'typeahead',
+        data() {
+            return {
+                inputText: ''
+            }
+        }
+    }
+
+</script>
